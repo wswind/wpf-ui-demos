f1267dc9245fec7cb5f6460e606613cbba7260e6
ws <ws_dev@163.com>
2021-07-06 16:28:15 +0800

    init

---
+++ MahAppsMetroDemo/Views/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+namespace MahAppsMetroDemo.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LaunchGitHubSite(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeployCupCakes(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
