0b4df2dfaa4502c5500f72a90746a19a7fea8d4b
ws <ws_dev@163.com>
2021-07-09 17:10:53 +0800

    change language

--- MaterialDesignInXamlDemo/App.xaml.cs
+++ MaterialDesignInXamlDemo/App.xaml.cs
@@ -13,5 +13,14 @@ namespace MaterialDesignInXamlDemo
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // test setup for Persian culture settings
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("zh-CN");
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("zh-CN");
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
+                        System.Windows.Markup.XmlLanguage.GetLanguage(System.Globalization.CultureInfo.CurrentCulture.IetfLanguageTag)));
+            base.OnStartup(e);
+        }
     }
 }
